1a3f8550c5dae23c6419f7fbfe3474bfec13805f
Nikhil Gargatte <enikhilraj@yahoo.com>
2023-07-13 23:40:29 +0100

    Create yser controler with request handling methods for CRUD operations.

---
+++ mobile-app-ws/src/main/java/edu/nick/ws/ui/controller/UserController.java
@@ -0,0 +1,30 @@
+package edu.nick.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.*;
+;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+	
+	@GetMapping
+	public String getUser() {
+		return "getUser was called.";
+	}
+	
+	@PostMapping
+	public String createUser() {
+		return "createUser was called.";
+	}
+
+	@PutMapping
+	public String updateUser() {
+		return "updateUser was called.";
+	}
+
+	@DeleteMapping
+	public String deleteUser() {
+		return "deleteUser was called.";
+	}
+
+}
