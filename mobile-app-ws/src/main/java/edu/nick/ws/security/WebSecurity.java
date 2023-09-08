9497d6e1f63e559dd9093660bd06c73ad999f8bb
Nikhil Gargatte <enikhilraj@yahoo.com>
2023-09-08 19:11:12 +0100

    Implement user sign-up by making the Web service endpoint public.

--- mobile-app-ws/src/main/java/edu/nick/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/edu/nick/ws/security/WebSecurity.java
@@ -1,38 +1,47 @@
 package edu.nick.ws.security;
 
+import edu.nick.ws.service.UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import edu.nick.ws.service.UserService;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+public class WebSecurity {
+    private UserService userDetailsService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    private final UserService userDetailsService;
-
-    public WebSecurity(BCryptPasswordEncoder bCryptPasswordEncoder, UserService userDetailsService) {
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/users").permitAll()
-                .antMatchers(HttpMethod.GET, "/users").permitAll()
-                .anyRequest().authenticated();
-    }
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity
+                .getSharedObject(AuthenticationManagerBuilder.class);
 
-    @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder.userDetailsService(userDetailsService)
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
                 .passwordEncoder(bCryptPasswordEncoder);
-    }
 
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+        httpSecurity.authenticationManager(authenticationManager);
+
+        httpSecurity
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        return httpSecurity.build();
+    }
 }
